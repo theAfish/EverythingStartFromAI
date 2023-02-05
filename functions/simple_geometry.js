61f323aaad01bc9339b6089a82503448fa533909
theNotFish <85424334+theAfish@users.noreply.github.com>
2023-02-05 21:29:18 +1100

    Create simple_geometry.js

---
+++ functions/simple_geometry.js
@@ -0,0 +1,11 @@
+// JavaScript file
+
+// Function that displays a message in the console
+function displayMessage() {
+  console.log("Hello, World!");
+}
+
+// Function that changes the background color of an element
+function changeBackgroundColor(elementId, color) {
+  document.getElementById(elementId).style.backgroundColor = color;
+}
