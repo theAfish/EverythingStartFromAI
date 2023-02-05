75e49df96b2125f8150bb653c2dde534ae7e22cd
theNotFish <85424334+theAfish@users.noreply.github.com>
2023-02-05 21:32:21 +1100

    Update simple_geometry.js

--- functions/simple_geometry.js
+++ functions/simple_geometry.js
@@ -9,3 +9,24 @@ function displayMessage() {
 function changeBackgroundColor(elementId, color) {
   document.getElementById(elementId).style.backgroundColor = color;
 }
+
+// Function that creates a circle
+function create_circle(radius, x, y, color) {
+  // Get reference to the canvas element
+  var canvas = document.getElementById("canvas");
+  
+  // Get the 2D rendering context
+  var context = canvas.getContext("2d");
+  
+  // Set the fill style to the specified color
+  context.fillStyle = color;
+  
+  // Start a new path
+  context.beginPath();
+  
+  // Draw an arc to create a circle
+  context.arc(x, y, radius, 0, 2 * Math.PI);
+  
+  // Fill the circle with the fill style
+  context.fill();
+}
